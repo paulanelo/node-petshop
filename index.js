180f1d1b520e488e6d8e8be5e1d467d7c68709cc
Paula Nelo <paulanelo1995@gmail.com>
2020-02-18 16:34:11 -0300

    identação e funcionalidade de adicionar um novo pet

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-const http = require('http');
-const { addPet, listPets } = require('./petshop');
+const http = require("http");
+const url = require("url");
+const { addPet, listPets } = require("./petshop");
 
 const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/html;charset=utf8');
-    switch(req.url) {
-        case '/criar':
-            return res.end('pets');
-        case '/listar':
-            return res.end(listPets())
-        default:
-            return res.end('Você está dentro do sistema petshop!');
-    }
+  res.setHeader("Content-Type", "text/html;charset=utf8");
+  const { name } = url.parse(req.url, true).query;
+  switch (true) {
+    case /\/criar.+/.test(req.url):
+      if (name) {
+        addPet(name);
+        return res.end(
+          `${name} foi adicionado(a). <a href="/listar">Ver lista</a>`
+        );
+      }
+    case /\/listar/.test(req.url):
+      return res.end(listPets());
+    default:
+      return res.end("Você está dentro do sistema petshop!");
+  }
 });
 
-server.listen(3000, 'localhost');
+server.listen(8000, "localhost");
