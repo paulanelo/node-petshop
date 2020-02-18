53d2c7390ddf4aace93fa073932946f8f80d8707
Paula Nelo <paulanelo1995@gmail.com>
2020-02-18 17:41:01 -0300

    funcionalidade de buscar pet pelo nome

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require("http");
 const url = require("url");
-const { addPet, listPets, deletePet } = require("./petshop");
+const { addPet, listPets, deletePet, findPet } = require("./petshop");
 
 const server = http.createServer((req, res) => {
   res.setHeader("Content-Type", "text/html;charset=utf8");
@@ -13,8 +13,24 @@ const server = http.createServer((req, res) => {
           `${name} foi adicionado(a). <a href="/listar">Ver lista</a>`
         );
       }
-    case /\/listar/.test(req.url):
+    case /\/listar$/.test(req.url):
+      console.log("listar");
       return res.end(listPets());
+    case /\/listar.+/.test(req.url):
+      console.log("with param");
+      if (name) {
+        const result = findPet(name);
+        console.log(result);
+        if (result) {
+          return res.end(
+            `---------------------------<br><strong>${result.id} Nome do pet:</strong> ${result.name}<br>---------------------------<br><a href="/listar">Ver lista</a>`
+          );
+        }
+        return res.end(
+          `Não foi possível excluir o pet ${name}. Verifique o nome e tente novamente. <a href="/listar">Ver lista</a>`
+        );
+      }
+
     case /\/deletar.+/.test(req.url):
       if (name) {
         if (deletePet(name)) {
--- petshop.js
+++ petshop.js
@@ -3,7 +3,7 @@ let petList = ['Bob', 'Tiger', 'July'];
 const addPet = (nome) => petList.push(nome);
 
 const listPets = () => {
-    return petList.reduce((a, c, id) => a += `<br><strong>${id+1} Nome do pet:</strong> ${c}<br>-------------------------`,'-------------------------');
+    return petList.reduce((a, c, id) => a += `<br><strong>${id+1} Nome do pet:</strong> ${c}<br>---------------------------`,'---------------------------');
 }
 
 const deletePet = (nome) => {
@@ -13,8 +13,17 @@ const deletePet = (nome) => {
     return false;
 }
 
+const findPet = (nome) => {
+    const id = petList.indexOf(nome)
+    if(id !== -1) {
+        return { id: id + 1, name: petList[id] };
+    }
+    return false;
+}
+
 module.exports = {
     addPet,
     listPets,
-    deletePet
+    deletePet,
+    findPet
 }
